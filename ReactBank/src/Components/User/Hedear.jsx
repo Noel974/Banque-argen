fb5f68671d8887c5d9610f69a8838026b3bfc450
Noel <72489142+Noel974@users.noreply.github.com>
2024-06-06 08:48:42 +0400

    Update Hedear.jsx

--- ReactBank/src/Components/User/Hedear.jsx
+++ ReactBank/src/Components/User/Hedear.jsx
@@ -1,15 +1,20 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Edit from '../../Composant/Edit';
-import { useSelector} from "react-redux";
-
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const user = useSelector(state => state.user.user);
   const [isEditing, setIsEditing] = useState(false);
   const [editableUser, setEditableUser] = useState(user);
 
+  useEffect(() => {
+    console.log("User from state:", user);
+    setEditableUser(user);
+  }, [user]);
+
   const handleEditEnd = (updatedUser) => {
     if (updatedUser) {
+      console.log("Updated User:", updatedUser);
       setEditableUser(updatedUser);
     }
     setIsEditing(false);
@@ -17,15 +22,17 @@ const Header = () => {
 
   return (
     <div className="header">
-      <h1>Welcome back<br />{editableUser ? `${editableUser.firstName} ${editableUser.lastName}` : 'User'}!</h1>
+      <h1>Welcome back<br />{editableUser ? `${editableUser.firstName} ${editableUser.lastName}` : 'Guest'}!</h1>
       {isEditing ? (
-        <Edit  className="edit-button" user={editableUser} onEditEnd={handleEditEnd} />
+        <Edit className="edit-button" user={editableUser} onEditEnd={handleEditEnd} />
       ) : (
-        <button className="edit-button" onClick={() => setIsEditing(true)}>Edit Name</button>
+        <button className="edit-button" onClick={() => {
+          console.log("Edit button clicked");
+          setIsEditing(true);
+        }}>Edit Name</button>
       )}
     </div>
   );
 };
-
 
 export default Header;
