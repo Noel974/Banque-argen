d29aa19be370b97064fe511ca510eb1152391d6d
Noel <72489142+Noel974@users.noreply.github.com>
2024-06-06 08:48:10 +0400

    Update Edit.jsx

--- ReactBank/src/Composant/Edit.jsx
+++ ReactBank/src/Composant/Edit.jsx
@@ -1,37 +1,60 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import { updateUserProfileAction } from "../Redux/Action/udapteprofilaction"; 
 import { useDispatch } from "react-redux";
 
 const Edit = ({ user, onEditEnd }) => {
-    const [firstName, setFirstName] = useState(user.firstName);
-    const [lastName, setLastName] = useState(user.lastName);
-    const dispatch = useDispatch();
-  
-    const handleSaveClick = () => {
-      const updatedUser = { firstName, lastName };
-      dispatch(updateUserProfileAction(updatedUser))
-        .then(() => {
-          onEditEnd(updatedUser);
-        })
-        .catch(error => {
-          console.error("Error updating user profile:", error);
-        });
-    };
+  const [firstName, setFirstName] = useState(user ? user.firstName : '');
+  const [lastName, setLastName] = useState(user ? user.lastName : '');
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    console.log("Edit component mounted with user:", user);
+  }, [user]);
 
-    return (
-        <div className="editName">
-
-<div className="inputBox">
-        <input type="text" placeholder={firstName} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <input type="text" placeholder={lastName} value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </div>
-        <div className="btnBox">
-        <button onClick={handleSaveClick}>Save</button>
-        <button onClick={onEditEnd}>Cancel</button>
-        </div>
-      </div>
-    );
+  const handleSaveClick = () => {
+    const updatedUser = { firstName, lastName };
+    console.log("Saving user:", updatedUser);
+    dispatch(updateUserProfileAction(updatedUser))
+      .then(() => {
+        console.log("User profile updated successfully");
+        onEditEnd(updatedUser);
+      })
+      .catch(error => {
+        console.error("Error updating user profile:", error);
+      });
   };
+
+  return (
+    <div className="editName">
+      <div className="inputBox">
+        <input 
+          type="text" 
+          placeholder="First Name" 
+          value={firstName} 
+          onChange={(e) => {
+            console.log("First name changed:", e.target.value);
+            setFirstName(e.target.value);
+          }} 
+        />
+        <input 
+          type="text" 
+          placeholder="Last Name" 
+          value={lastName} 
+          onChange={(e) => {
+            console.log("Last name changed:", e.target.value);
+            setLastName(e.target.value);
+          }} 
+        />
+      </div>
+      <div className="btnBox">
+        <button onClick={handleSaveClick}>Save</button>
+        <button onClick={() => {
+          console.log("Cancel button clicked");
+          onEditEnd();
+        }}>Cancel</button>
+      </div>
+    </div>
+  );
+};
 
 export default Edit;
